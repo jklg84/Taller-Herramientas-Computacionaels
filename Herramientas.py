35c2462c94e0dcf6ea4aab8e6f7485c59019730f
jklg84 <86082270+jklg84@users.noreply.github.com>
2021-06-18 15:51:20 -0500

    Update Herramientas.py

--- Herramientas.py
+++ Herramientas.py
@@ -1,3 +1,4 @@
+##Esta función se encarga de calcular el área del triangulo
 def CalcularArea ():
     print("Calcular el área de un triangulo")
     print("Ejemplo, un triangulo con base 50 y altura 30")
@@ -5,23 +6,25 @@ def CalcularArea ():
     base=50
     areaej=(base*altura)/2
     print(areaej)
-    a=int(input("Escribe el primer número "))
-    b=int(input("Escribe el segundo número "))
+    a=int(input("Digite el primer número "))
+    b=int(input("Digite el segundo número "))
     area=(a*b)/2
     print("Su respuesta es", area)
 CalcularArea()
 
 #2
+##Esta función se encarga de hallar la nota final de un estudiante
 def CalcularNotaF():
     print("Calcular nota final")
-    a=float(input("Escriba nota parcial uno " ))
-    b=float(input("Escriba nota parcial dos " ))
-    c=float(input("Escriba nota taller " ))
-    d=float(input("Escriba nota proyecto " ))
+    a=float(input("Digite nota parcial uno " ))
+    b=float(input("Digite nota parcial dos " ))
+    c=float(input("Digite nota taller " ))
+    d=float(input("Digite nota proyecto " ))
     notafinal=((a/100)*25+(b/100)*25+(c/100)*20+(d/100)*30)
     print("Su nota final es", notafinal)
 CalcularNotaF()
 #3
+##Esta función se encarga de transformar los grados de celsius a Farenheit
 def transformar():
     print("Transformar grados Celsius a Farenheit")
     a=float(input("Escriba los grados Celsius "))
@@ -29,6 +32,7 @@ def transformar():
     print("En Farenheit equivale a", farenheit)
 transformar()
 #4
+#Esta función cambia el valor de los COP por diferentes monedas alrededor del mundo
 def cambio():
     print("Casa de Cambio")
     n=float(input("Digite el monto en (COP) a cambiar "))
